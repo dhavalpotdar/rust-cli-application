0087b19798e34d04c4475e4ee59be883fb5d0cfe
Dhaval Potdar <dhavalpotdar@gmail.com>
2023-10-29 18:36:58 -0400

    Added rust installation command to setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ make install
 
 #append it to bash so every shell launches with it 
 echo 'source ~/.venv/bin/activate' >> ~/.bashrc
+
+## Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y 
+source "$HOME/.cargo/env"
